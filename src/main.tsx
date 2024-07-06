21507e5d2b34b06df8224a95a0c94b7596e972b6
Azafman <0000880867@senaimgaluno.com.br>
2024-07-06 11:32:19 -0300

    starting login development

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import store from './redux/store';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Login } from './components/Auth/Login.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>,
 )
